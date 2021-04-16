febefcf708583f25bbdeb0d9ed139118c1e98861
Gregory <garthus@adminux.fr>
2021-04-16 16:20:51 +0200

    add(): download fonctionnality

--- Download/gtf.sh
+++ Download/gtf.sh
@@ -9,17 +9,23 @@
 #						#
 #################################################
 
- 
-EP=$(curl -s https://eztv.re/search/$1 | grep $1 | sed "s/.*title=\"//g" | grep "\[eztv\]" | sed "s/\ \[eztv\].*$/\[eztv.re\].mkv/" | grep 264 | sed -e "s/\s/\./g")
-i=1
+# Retrieve and format the Episode List
 
+EP=$(curl -s https://eztv.re/search/"$1" | grep "$1" | sed "s/.*title=\"//g" | grep "\[eztv\]" | sed "s/\ \[eztv\].*$/\[eztv.re\].mkv/" | grep 264 | sed -e "s/\s/\./g" | sort -n) 
+
+# print the episode List
+
+i=1
 for VAL in $EP
 do
 	echo "$i - $VAL"
 	i=$((i+1))
 done
 echo ''
-read -p "Wich Episode Do you Want ?: " CHOOSED
+read -rp "Wich Episode ?: " CHOOSED
 echo ''
 EPCHOOSED=$(echo $EP | cut -d " " -f $CHOOSED)
-echo $EPCHOOSED
+echo "$EPCHOOSED"
+
+# Download the .torrent file
+curl -sgO "https://zoink.ch/torrent/$EPCHOOSED.torrent"
